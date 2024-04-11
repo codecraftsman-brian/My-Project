9500ef95aa536424f92e7e03c16ee9019ee0c2a0
codecraftsman-brian <engkariukingugi@gmail.com>
2024-04-11 14:28:55 -0400

    Added treasure_map assignment script

--- Learning python/004 Randomisation and Lists/03. Treasure_map.py
+++ Learning python/004 Randomisation and Lists/03. Treasure_map.py
@@ -1,16 +1,17 @@
 #Don't change the code below
-
 row1 = ['✨','✅','❤️']
 row2 = ['✨','✅','❤️']
 row3 = ['✨','✅','❤️']
 map = [row1, row2, row3]
-#print(f"{row1}\n {row2} \n {row3}")
-position = input("Where do you want to put the treasure?")
-
-#Don't change the code above
+print(f"{row1}\n {row2} \n {row3}")
+position = input("Where do you want to put the treasure\n")
+#Don't change the code above2
 
 #write your code below this line
-row1.pop(position[0])
-row1.insert(position[0],'X')
-print(row1)
+vertical = int(position[0])
+horizontal = int(position[1])
+
+map[horizontal-1][vertical-1] = 'X'
+
+print(f"{row1}\n {row2} \n {row3}")
 
